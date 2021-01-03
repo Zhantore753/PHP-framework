69860c13fef2ab68839c8ec301a96156fc8cefdf
Zhantore <753gg@mail.ru>
2021-01-03 11:20:00 +0600

    start

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+define('VG_ACCESS', true);
+
+header('Content-Type:text/html;charset-utf-8');
+session_start();
+
+require_once 'config.php';
+require_once 'core/base/settings/internal_settings.php';
+
+use core\base\exceptions\RouteException;
+
+try{
+    (new A());
+}catch(RouteException $e){
+    exit($e->getMessage());
+}
