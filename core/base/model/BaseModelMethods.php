40aa8589cee3913fad1d9cd6efce15d07785a6e6
Zhantore <753gg@mail.ru>
2021-01-20 15:41:28 +0600

    update 25 - universal method of adding to the database, part 2

--- core/base/model/BaseModelMethods.php
+++ core/base/model/BaseModelMethods.php
@@ -250,9 +250,20 @@ namespace core\base\model;
 
              foreach ($files as $row => $file){
 
+                 $insert_arr['fields'] .= $row . ',';
+
+                 if(is_array($file)) $insert_arr['values'] .= "'" . addslashes(json_encode($file)) . "',";
+                 else $insert_arr['values'] .= "'" . addslashes($file) . "',";
+
              }
 
          }
+
+         if($insert_arr){
+             foreach ($insert_arr as $key => $arr) $insert_arr[$key] = rtrim($arr,',');
+         }
+
+         return $insert_arr;
 
      }
 
